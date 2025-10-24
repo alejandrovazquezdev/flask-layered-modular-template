edf1fc415fed66ed7aaf3f6a20750290aee6cee9
Alejandro Isaac Vazquez Lopez <alejandroisaacvazquezlopez@gmail.com>
2025-10-24 01:15:34 -0600

    Lección 9: Logs en Flask - Configuración del sistema de logging
    - Importar logging y SMTPHandler para manejo de logs y envío de emails
    - Crear función verbose_formatter() para formato detallado de logs
    - Crear función mail_handler_formatter() para formato de emails de error
    - Crear función configure_logging(app) que configura handlers por entorno:
      * LOCAL/TESTING/DEVELOPMENT: StreamHandler con nivel DEBUG
      * STAGING: StreamHandler con nivel INFO
      * PRODUCTION: StreamHandler (INFO) + SMTPHandler (ERROR)
    - Agregar loggers de sqlalchemy para ver queries de base de datos
    - Integrar configure_logging() en create_app() después de cargar configuración
    - Agregar parámetros MAIL_* a config/default.py
    - Agregar logger personalizado en app/public/routes.py
    - Logs en index(): current_app.logger.info() + logger.info()
    - Logs en show_post(): info() y debug() con distintos niveles
    
    Sistema de logging con niveles: DEBUG < INFO < WARNING < ERROR < CRITICAL
    Handlers: StreamHandler (consola) y SMTPHandler (email en producción)
    Formatters personalizados para cada tipo de salida

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,78 @@
+import logging
+from logging.handlers import SMTPHandler
+
 from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 login_manager = LoginManager()
 db = SQLAlchemy()
+
+
+def verbose_formatter():
+    return logging.Formatter(
+        '[%(asctime)s.%(msecs)d]\t %(levelname)s \t[%(name)s.%(funcName)s:%(lineno)d]\t %(message)s',
+        datefmt='%d/%m/%Y %H:%M:%S'
+    )
+
+
+def mail_handler_formatter():
+    return logging.Formatter(
+        '''
+            Message type:       %(levelname)s
+            Location:           %(pathname)s:%(lineno)d
+            Module:             %(module)s
+            Function:           %(funcName)s
+            Time:               %(asctime)s.%(msecs)d
+
+            Message:
+
+            %(message)s
+        ''',
+        datefmt='%d/%m/%Y %H:%M:%S'
+    )
+
+
+def configure_logging(app):
+
+    # Elimina los manejadores por defecto de la app
+    del app.logger.handlers[:]
+
+    loggers = [app.logger, logging.getLogger('sqlalchemy')]
+    handlers = []
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(verbose_formatter())
+
+    if (app.config['APP_ENV'] == app.config['APP_ENV_LOCAL']) or (
+            app.config['APP_ENV'] == app.config['APP_ENV_TESTING']) or (
+            app.config['APP_ENV'] == app.config['APP_ENV_DEVELOPMENT']):
+        console_handler.setLevel(logging.DEBUG)
+        handlers.append(console_handler)
+    elif app.config['APP_ENV'] == app.config['APP_ENV_STAGING']:
+        console_handler.setLevel(logging.INFO)
+        handlers.append(console_handler)
+    elif app.config['APP_ENV'] == app.config['APP_ENV_PRODUCTION']:
+        console_handler.setLevel(logging.INFO)
+        handlers.append(console_handler)
+
+        mail_handler = SMTPHandler((app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                                   app.config['DONT_REPLY_FROM_EMAIL'],
+                                   app.config['ADMINS'],
+                                   '[Error][{}] La aplicación falló'.format(app.config['APP_ENV']),
+                                   (app.config['MAIL_USERNAME'],
+                                    app.config['MAIL_PASSWORD']),
+                                   ())
+        mail_handler.setLevel(logging.ERROR)
+        mail_handler.setFormatter(mail_handler_formatter())
+        handlers.append(mail_handler)
+
+    # Asociamos cada uno de los handlers a cada uno de los loggers
+    for l in loggers:
+        for handler in handlers:
+            l.addHandler(handler)
+        l.setLevel(logging.DEBUG)
+        l.propagate = False
 
 
 def register_error_handlers(app):
@@ -26,6 +95,8 @@ def create_app(settings_module):
         app.config.from_pyfile('config-testing.py', silent=True)
     else:
         app.config.from_pyfile('config.py', silent=True)
+    
+    configure_logging(app)
     
     # Inicializar extensiones
     db.init_app(app)
